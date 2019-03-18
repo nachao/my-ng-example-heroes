1e4a71206d83fe0039296b74c3c6e32b62755cb6
nachao <357586693@qq.com>
2019-03-19 00:37:23 +0800

    feat: compelte

--- src/app/in-memory-data.service.ts
+++ src/app/in-memory-data.service.ts
@@ -1,9 +1,9 @@
-import { Injectable } from '@angular/core';
 import { InMemoryDbService } from 'angular-in-memory-web-api';
 import { Hero } from './hero';
+import { Injectable } from '@angular/core';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class InMemoryDataService implements InMemoryDbService {
   createDb() {
